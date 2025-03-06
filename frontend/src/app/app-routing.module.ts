3e9acff042efd2f65cb48332aafaca4e95ebf2b9
os2594 <ocarboneylopez@unomaha.edu>
2025-03-06 15:12:21 -0600

    created main-dashboard component and added title

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+
+const routes: Routes = [
+  {
+    path: 'main-dashboard',
+    component: MainDashboardComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/main-dashboard/main-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-dashboard',
+  standalone: false,
+  templateUrl: './main-dashboard.component.html',
+  styleUrl: './main-dashboard.component.css'
+})
+export class MainDashboardComponent {
+
+}
