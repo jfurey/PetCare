13cf8612abce7a4ecc09b4267da3c51ee9480e9e
allisoncoates <coates.allison@outlook.com>
2025-03-31 20:28:24 -0500

    Adding code for get request for appointments and activities

--- backend/activities.py
+++ backend/activities.py
@@ -1,20 +1,27 @@
 # Activities API routes
 
-
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, current_app
+import MySQLdb.cursors
 
 bp = Blueprint("activities", __name__, url_prefix="/activities")
 
 
 @bp.get("", strict_slashes=False)
 def get_activities():
-    # TODO: return list of activities
-    return []  # returning a blank list for testing
+    # connect to database and retrieve activities
+    cursor = current_app.extensions['mysql'].connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT activity_id, pet_id, activity_type, other_activity_type, duration_minutes, activity_date, notes FROM activity_logs")
+
+    activity_list = cursor.fetchall()
+
+    # convert to dictionary
+    activities = {activity['activity_id']: activity for activity in activity_list}
+    return jsonify(activities)
 
 
 @bp.post("", strict_slashes=False)
 def add_activity():
-    # TODO: create a activity
+    # TODO: create an activity
     return {}
 
 
